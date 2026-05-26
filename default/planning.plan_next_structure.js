dc2bd941b06103121e625ad02f6de1819248c154
Killian Woods <drabsage@gmail.com>
2026-05-26 01:37:42 -0400

    holy shit im generating roads! that was really hard...

--- default/planning.plan_next_structure.js
+++ default/planning.plan_next_structure.js
@@ -2,6 +2,7 @@ const {StructurePriority} = require('./planning.priority')
 const {spiralOutward} = require('./utilities.spiral')
 const {hasBlockedNeighbor} = require('./utilities.has_blocked_neighbor');
 const { ROOM_SIZE } = require('./consts');
+const {roadPlannerCoords} = require('./utilities.road_planner')
 
 /**
 * @param {Room} room
@@ -64,16 +65,61 @@ function __planUtility(room, structure) {
 
 }
 
+/**
+* @param {Room} room
+*/
 function _planExtension(room) {
-    __planUtility(room, STRUCTURE_EXTENSION)
+    __planUtility(room, STRUCTURE_EXTENSION);
 }
 
+/**
+* @param {Room} room
+*/
 function _planContainer(room) {
-    __planUtility(room, STRUCTURE_CONTAINER)
+    __planUtility(room, STRUCTURE_CONTAINER);
 }
 
+/**
+* @param {Room} room
+*/
 function _planRoads(room) {
-    return;
+
+    // initialize
+    if (!Memory.road_planner || !Memory.road_planner.coords || Memory.road_planner.index === undefined) {
+        Memory.road_planner = {
+            coords: roadPlannerCoords(room),
+            index: 0
+        }
+        console.log("`Memory.road_planner` was generated.")
+    } 
+
+    // reset when finished
+    if (Memory.road_planner.index >= Memory.road_planner.coords.length) {
+        Memory.road_planner.coords = roadPlannerCoords(room);
+        Memory.road_planner.index = 0;
+        console.log("`Memory.road_planner` was regenerated.")
+    }
+
+    const raw = Memory.road_planner.coords[Memory.road_planner.index];
+    const pos = new RoomPosition(raw.x, raw.y, raw.roomName);
+
+    if (!hasRoadOrSite(pos)) {
+        pos.createConstructionSite(STRUCTURE_ROAD);
+    }
+
+    Memory.road_planner.index++;
+    
+}
+
+function hasRoadOrSite(pos) {
+    const structures = pos.lookFor(LOOK_STRUCTURES);
+    const sites = pos.lookFor(LOOK_CONSTRUCTION_SITES);
+
+    return (
+        structures.some(s => s.structureType === STRUCTURE_ROAD) ||
+        sites.some(s => s.structureType === STRUCTURE_ROAD)
+    )
+
 }
 
 module.exports = {planNextStructure};
---
+++ default/utilities.road_planner.js
@@ -0,0 +1,71 @@
+const {ROOM_SIZE} = require('./consts')
+const { FindSafeSources } = require('./utilities.find_safe_sources')
+
+/**
+ * 
+ * @param {Room} room 
+ */
+function roadPlannerCoords(room) {
+    // get array of all structures that need paths between them
+    // every structure in this array will have a path to every other structure
+
+    const coords = [];
+
+    const _sources = FindSafeSources(room);
+    const _controller = [room.controller];
+    const _spawns = room.find(FIND_MY_SPAWNS);
+    const importantStructures = [..._sources, ..._controller, ..._spawns];
+
+    for (let i = 0; i < importantStructures.length; i++) {
+        for (let j = i + 1; j < importantStructures.length; j++) {
+            const path = PathFinder.search(importantStructures[i].pos, importantStructures[j].pos, {range: 1}).path;
+            for (let pos of path) {
+                coords.push(pos);
+            }
+        }
+    }
+
+    //  coords one at a time
+
+    const containersAndExtensions = room.find(FIND_STRUCTURES, { filter: (structure) => {
+        return structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_EXTENSION
+    }});
+    // THEN, get array of containers and extensions
+    //  coords one at a time for each square around them
+    for (let struct of containersAndExtensions) {
+        for (let dx = -1; dx <= 1; dx++) {
+            for (let dy = -1; dy <= 1; dy++) {
+                const x = struct.pos.x + dx;
+                const y = struct.pos.y + dy;
+                if (dx === 0 && dy === 0) {
+                    continue;
+                } else if (
+                    x < 0 ||
+                    x > ROOM_SIZE - 1 ||
+                    y < 0 ||
+                    y > ROOM_SIZE - 1
+                ) {
+                    continue;
+                }
+
+                coords.push(new RoomPosition(x, y, room.name));
+            }
+        }
+    }
+
+    const seen = new Set();
+    const unique = [];
+
+
+    for (const pos of coords) {
+        const key = `${pos.roomName}:${pos.x}:${pos.y}`;
+
+        if (!seen.has(key)) {
+            seen.add(key);
+            unique.push(pos);
+        }
+    }
+    return unique;
+}
+
+module.exports = {roadPlannerCoords};
